23523eef7e8ea721a306754a9fafba5c582aa5bf
忘忧北萱草 <wybxc@qq.com>
2025-01-21 17:57:55 +0800

    feat: 添加 Tailwind CSS 配置和相关文件，重构项目结构

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+	return (
+		<div className="p-6">
+			<h1 className="text-xl">Rsbuild with React</h1>
+			<p>Start building amazing things with Rsbuild.</p>
+		</div>
+	);
+};
+
+export default App;
